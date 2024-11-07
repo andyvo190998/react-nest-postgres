5f93803f53788551a09480bf3dcbce59c0b626d8
AndyVo <andy.vo@kiutra.com>
2024-11-07 14:58:49 +0100

    test ci

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+	console.log("hello world");
 	await app.listen(3333);
 }
 bootstrap();
