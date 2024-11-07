c819970b80da6bc0c589381be6bf092966d960b3
AndyVo <andy.vo@kiutra.com>
2024-11-07 16:20:32 +0100

    trying to test ci

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
-	console.log("hello world");
+	console.log("hello worldqwe");
 	await app.listen(3333);
 }
 bootstrap();
